1ece083d7fcdfdea2a27c34d6aa6608cb4f3b310
KonstantsiyaP <kasya.aysak@gmail.com>
2020-03-13 12:42:51 +0200

    CompareSteps completed

--- src/test/java/stepDefinitions/CompareSteps.java
+++ src/test/java/stepDefinitions/CompareSteps.java
@@ -1,12 +1,10 @@
 package stepDefinitions;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import pagesobjects.AgePage;
 import pagesobjects.EditPage;
@@ -24,12 +22,13 @@ public class CompareSteps {
         editPage = PageFactory.initElements(Hooks.driver, EditPage.class);
         agePage = PageFactory.initElements(Hooks.driver, AgePage.class);
     }
-    @Given("^I log in to web app with e-mail \"([^\"]*)\" and password \"([^\"]*)\"$")
-    public void iLogInToWebAppWithEMailAndPassword(String email, String passWord) throws Throwable {
 
+    @Given("^I log in to web app with e-mail \"([^\"]*)\", password \"([^\"]*)\"$")
+    public void iLogInToWebAppWithEMailPassword(String email, String passWord) throws Throwable {
         driver.get("http://demoshop24.com/");
         agePage.loggingIn(email, passWord);
     }
+
 
     @When("^I go to Edit Account section$")
     public void i_go_to_Edit_Account_section() throws Throwable {
@@ -39,9 +38,11 @@ public class CompareSteps {
 
     @Then("^I compare first name \"([^\"]*)\", second name \"([^\"]*)\", e-mail \"([^\"]*)\", phone \"([^\"]*)\"$")
     public void iCompareFirstNameSecondNameEMailPhone(String fName, String lName, String email, String phone) throws Throwable {
-        Assert.assertEquals(editPage.getEdFirstName().getAttribute(VALUE),fName);
-        Assert.assertEquals(editPage.getEdSecondName().getAttribute(VALUE),lName);
-        Assert.assertEquals(editPage.getEdEmail().getAttribute(VALUE),email);
-        Assert.assertEquals(editPage.getEdPhoneNumber().getAttribute(VALUE),phone);
+        Assert.assertEquals(editPage.getEdFirstName().getAttribute(VALUE), fName);
+        Assert.assertEquals(editPage.getEdSecondName().getAttribute(VALUE), lName);
+        Assert.assertEquals(editPage.getEdEmail().getAttribute(VALUE), email);
+        Assert.assertEquals(editPage.getEdPhoneNumber().getAttribute(VALUE), phone);
     }
+
+
 }
